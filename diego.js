86743bee5ed7c40c0bbda716351f75d15494b421
diegofrabasa <diegofrabasa@gmail.com>
2019-05-24 16:24:31 -0500

    funcion de crear eventos lista

--- diego.js
+++ diego.js
@@ -2,10 +2,16 @@ const btnCrearEvento = document.getElementById('btnCrearEvento')
 const inputNombreEvento = document.getElementById('inputNombreEvento')
 const inputDescripcionEvento = document.getElementById('inputDescripcionEvento')
 
-function _(el){
-    return document.getElementById(el);
-    // _('id').innerHTML - _('id').value
-  }
+const btnRegistrarse = document.getElementById('btnRegistrarse')
+
+let id = 1
+let a = 0
+
+const asistentes = (id,a) => {
+    let asistentes = document.getElementById(id)
+     a++
+    asistentes.innerHTML = 'Regristraste (' + a + ')'
+}
 
 const crearEvento = () => {
 
@@ -16,7 +22,8 @@ const crearEvento = () => {
     let h5Card = document.createElement('h5')
     let pCard = document.createElement('p')
     let aCard = document.createElement('a')
-    let spanCard = document.createElement('span')
+    let pIdCard = document.createElement('span')
+    // let pAsistentesCard = document.createElement('p')
 
     //Obtener valores del formulario de el evento
     let inputNombreEventoValue = inputNombreEvento.value
@@ -29,16 +36,22 @@ const crearEvento = () => {
     imgCard.setAttribute("alt", "Card image cap")
     divCardBody.setAttribute("class", "card-body")
     pCard.setAttribute("class", "card-text")
-    aCard.setAttribute("href", "#")
+    // aCard.setAttribute("href", "#")
+    aCard.setAttribute("id", 'link'+id)
     aCard.setAttribute("class", "btn btn-primary")
+    aCard.setAttribute("onclick", "asistentes(id,a)")
+    // pAsistentesCard.setAttribute("id", id)
     
     //Insertar el contenido a cada parte
     // h5Card.innerHTML = 'Card title'
     h5Card.innerHTML = inputNombreEventoValue
     // pCard.innerHTML = 'Some quick example text to build on the card title and make up the bulk of the cards content.'
     pCard.innerHTML = inputDescripcionEventoValue
-    aCard.innerHTML = 'Registrarse'
-    
+    aCard.innerHTML = 'Regristraste (' + a + ')'
+    pIdCard.innerHTML = '<br>Id Evento: ' + id
+    // pAsistentesCard.innerHTML = 'No. asistentes'
+
+    id++
 
     //acomodar el contenido
     divCard.appendChild(imgCard)
@@ -46,8 +59,12 @@ const crearEvento = () => {
     divCardBody.appendChild(h5Card)
     divCardBody.appendChild(pCard)
     divCardBody.appendChild(aCard)
+    divCardBody.appendChild(pIdCard)
+    // divCardBody.appendChild(pAsistentesCard)
     
     document.getElementById('sec3-cards').appendChild(divCard)
 }
+
+
 
 btnCrearEvento.addEventListener('click',crearEvento)
